484184e41eaa577c56b163217e555ef90b0d16a9
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-09 06:16:55 +0300

    [update] only paermitted params when creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.permit(:name, :email, :password, :password_confirmation)
     end
 end
