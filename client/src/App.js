a72242c37972d1a25a94eaf5585bc18e8b19a178
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-09 15:10:11 +0300

    [create] navbar component and add boostrap md

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      {/* 
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/games" element={<Games />} />
+        <Route exact path="/games/:gameID" element={<Game />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes> */}
     </div>
   );
 }
