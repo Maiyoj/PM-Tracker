46e0753c73b08345be284552c39d1f26853e40c8
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-12 09:28:20 +0300

    [add] and [update] users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :tickets
   resources :projects
   resources :users, only:[:create, :show, :index]
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_045426) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_060331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "projectname"
     t.string "description"
+    t.integer "client_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
