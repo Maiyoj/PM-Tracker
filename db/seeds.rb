b6e80e2da21eb726bcdd3b798c3e21446985a223
Maiyoj <valentinemaiyo1995@gmail.com>
2022-10-13 17:33:10 +0300

    [change] seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 #create ADmin user
 
-User.create!(name: "admin", email: "admin@gmail.com", password: "12345678" ,admin: true)
+User.create!(name: "Valentine", email: "val@gmail.com", password: "12345678" ,admin: true)
